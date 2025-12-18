abf16b3f70f4763b3abc0a862dac4c026695979d
Ahmer Abid <ahmerabid39@gmail.com>
2025-12-18 10:42:52 +0500

    Day 4: Functions practice

---
+++ day4_functions.py
@@ -0,0 +1,48 @@
+#Q :Write a function that takes a list as input and prints the length of the list.
+# cities = ["new york", "washington", "berlin", "madrid","barcelona"]
+# nums = [2, 13, 14, 16, 20, 25, 30]
+
+# def print_lists(list):
+#     print(len(list))
+
+# print_lists(cities)
+# print_lists(nums)
+
+
+
+cities = ["new york", "washington", "berlin", "madrid","barcelona"]
+nums = [2, 13, 14, 16, 20, 25, 30]
+
+def print_len(list):
+    print(len(list))
+def print_list(items):
+    for item in items:
+        print(item, end=" ")
+
+
+print_list(cities)
+# print()    
+
+
+#Q :Write a Python function that returns the maximum number from a list without using built in functions.
+# def find_max(nums):
+#     max_num = nums[0]
+#     for n in nums:
+#         if n > max_num:
+#             max_num = n
+#     return max_num
+
+# print(find_max([3, 9, 1, 7]))
+
+
+# def check_num(num):
+#     num = int(input("write a number: "))
+#     if num % 2 == 0:
+#         print("even")
+#     else:
+#         print("odd")
+
+#     check_num(num)
+
+
+    
